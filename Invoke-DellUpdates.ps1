cc66b367645b55cd308a55056103269f27b41c66
ajh0912 <git@ajh0912.com>
2022-05-11 22:14:14 +0100

    Add Invoke-DellUpdates.ps1

---
+++ Invoke-DellUpdates.ps1
@@ -0,0 +1,70 @@
+<#
+.SYNOPSIS
+Performs updates available from Dell Command Update. Drivers, firmware & BIOS/UEFI etc.
+
+.DESCRIPTION
+Can be executed from an RMM that sets environment variables to pass information to the script.
+The values of the parameters will come from environment variables, unless the parameters are explicitly provided. 
+Dell Command Update must already be present (installed via .exe, not via Microsoft Store / UWP).
+
+If a BIOS/UEFI password is present on the current computer, then $biosPasswordNeeded should be set to $true,
+and the (already encrypted) password should be provided in $biosPwEncrypted.
+
+.LINK
+https://www.dell.com/support/manuals/en-uk/command-update/dellcommandupdate_rg/dell-command-%7C-update-command-line-interface?guid=guid-c8d5aee8-5523-4d55-a421-1781d3da6f08&lang=en-us
+#>
+
+param (
+    [Parameter()][bool]$autoReboot = $env:autoReboot,
+    [Parameter()][bool]$biosPasswordNeeded = $env:biosPasswordNeeded,
+    [Parameter()][string]$biosPwEncrypted = $env:biosPwEncrypted
+)
+
+$dateTime = Get-Date -Format FileDateTimeUniversal
+$programFiles = if ([Environment]::Is64BitOperatingSystem) {
+    ${env:ProgramFiles(x86)}
+}
+else {
+    $env:ProgramFiles
+}
+$dcuExePath = Join-Path -Path $programFiles -ChildPath 'Dell\CommandUpdate\dcu-cli.exe'
+$standardOutputPath = Join-Path -Path $env:TEMP -ChildPath "$dateTime-DellUpdates-Output.txt"
+
+# Create a list to contain our arguments for Start-Process
+[System.Collections.Generic.List[String]]$argumentList = @("/applyUpdates -autoSuspendBitLocker=enable")
+switch ($true) {
+    # Environment variables are being passed from RMM via either site-wide variables or script variables
+    { $autoReboot -eq $true } {
+        # Add to the list of Start-Process arguments
+        [void]$argumentList.Add("-reboot=enable")
+    }
+    { $biosPasswordNeeded -eq $true } {
+        # See https://www.dell.com/support/kbdoc/en-uk/000187573/bios-password-is-not-included-in-the-exported-configuration-of-dell-command-update
+        # Add to the list of Start-Process arguments
+        [void]$argumentList.Add("-encryptionkey=`"MyEncryptionKey01`" -encryptedpassword=`"$biosPwEncrypted`"")
+    }
+}
+$dcuParams = @{
+    FilePath               = $dcuExePath
+    Verbose                = $true
+    Wait                   = $true
+    # Start-Process cannot redirect standard output to a variable, so it is instead written to a file and read back
+    RedirectStandardOutput = $standardOutputPath
+    WindowStyle            = 'Hidden'
+    ArgumentList           = $argumentList -join ' '
+}
+
+try {
+    # For debugging you can tail the standard output as it runs with:
+    # Get-Content -Tail 100 -Wait $env:TEMP\*DellUpdates-Output*
+    Start-Process @dcuParams
+    Write-Host 'Updates log:'
+    Write-Host (Get-Content -Path $standardOutputPath -Raw)
+}
+catch {
+    # Writing errors to standard error and standard output as some RMM platforms split these into different windows
+    Write-Error $_
+    Write-Host 'Error: Dell Command Update did not finish successfully, updates log:'
+    Write-Host (Get-Content -Path $standardOutputPath -Raw)
+    Write-Error "Error: Dell Command Update did not finish successfully" -ErrorAction Stop
+}
